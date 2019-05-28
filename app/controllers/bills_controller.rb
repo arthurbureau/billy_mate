ab9636dd660c5299dd7f175991beed0a3d36fb75
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-05-28 16:01:31 +0200

    bill et profile controller

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,4 @@
 class BillsController < ApplicationController
-  # before_action :find_flat, only: [:new, :create]
 
   def index
     @bills = Bill.all
@@ -25,22 +24,37 @@ class BillsController < ApplicationController
   end
 
   def edit
-
+    @bill = Bill.find(params[:id])
+    if current_user == @bill.user
+    else
+      flash[:alert] = "Seule le titulaire du contrat peut modifier une facture"
+      redirect_to profile_path
+    end
   end
 
   def update
-
+    @bill = Bill.find(params[:id])
+    if @bill.update(set_bill_params)
+      flash[:notice] = "Yay! 🎉 you have successfully udpate your bill."
+      redirect_to profile_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @bill = Bill.find(params[:id])
+    if current_user == @bill.user
+      @bill.destroy
+      flash[:alert] = "Tu viens de supprimer une facture"
+      redirect_to profile_path
+    else
+      flash[:alert] = "Seule le titulaire du contrat peut supprimer une facture"
+      redirect_to profile_path
+    end
   end
 
   private
-
-  # def find_flat
-  #   @flat = Flat.find(params[:flat_id])
-  # end
 
   def set_bill_params
     params.require(:bill).permit(:category, :provider, :amount, :payment_date, :contract_picture)
