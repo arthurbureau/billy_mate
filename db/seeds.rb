f8a88ead6b5bc9bae805e2b9d441ef81d754338f
anne sophie ducamin <annesophie.ducamin@gmail.com>
2019-06-03 15:51:30 +0200

    remboursement

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,6 +53,10 @@ elect = Bill.create!(flat: flat1, category: 'Electricité', provider: 'EDF', use
 gaz = Bill.create!(flat: flat1, category: 'Gaz', provider: 'Gaz de Bordeaux', user: user2, amount_cents: 9000, payment_date: Date.new(2019, 5, 15))
 eau = Bill.create!(flat: flat1, category: 'Eau', provider: 'Bordeaux Métropole', user: user3, amount_cents: 2000, payment_date: Date.new(2019, 5, 20))
 
+elect1 = Bill.create!(flat: flat1, category: 'Electricité', provider: 'EDF', user: user1, amount_cents: 6000, payment_date: Date.new(2019, 6, 23))
+gaz1 = Bill.create!(flat: flat1, category: 'Gaz', provider: 'Gaz de Bordeaux', user: user2, amount_cents: 9000, payment_date: Date.new(2019, 6, 15))
+eau1 = Bill.create!(flat: flat1, category: 'Eau', provider: 'Bordeaux Métropole', user: user3, amount_cents: 2000, payment_date: Date.new(2019, 6, 20))
+
 #gérer contracts pictures avec Cloudinary
 
 puts 'Creation transactions'
