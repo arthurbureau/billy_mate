a20538efefceab2c965c3e43dd8d95c894e62dbc
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-06-06 10:51:01 +0200

    metatag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["www.billy-mate.pro"] || "localhost:3000" }
-end
-
+    { host: ENV["www.billy-mate.pro"] || "localhost:3000" }
+  end
 end
