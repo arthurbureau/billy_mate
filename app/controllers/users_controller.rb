5f5f2569abc8ec1df2a5670712075c97c91901dd
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-06-06 16:06:32 +0200

    small modif

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,15 @@ class UsersController < ApplicationController
     @flat = Flat.find_by(token: user_params[:token])
 
     if current_user.flat_id.present?
-      flash[:notice] = "Tu fais déjà parti d'une coloc!  "
+      flash[:notice] = "Tu fais déjà parti d'une coloc! "
       redirect_to flat_path(@flat)
     else
       current_user.flat = @flat
       if @flat && current_user.save
-        flash[:notice] = "Bienvenue dans ta coloc!  "
+        flash[:notice] = "😎 Bienvenue dans ta coloc!"
         redirect_to bills_path
       else
-        flash[:alert] = "Votre lien ne fonctionne pas.  "
+        flash[:alert] = "Le lien ne fonctionne pas."
         render "flats/join_flat"
       end
     end
