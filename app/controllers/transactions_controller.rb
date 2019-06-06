cb03069bcb329103fe8eae3b7fb1cd06bafdc821
Arthur Bureau <arthur.bureau@kedgebs.com>
2019-06-06 18:35:47 +0200

    lydia implemnt

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,26 +4,35 @@ class TransactionsController < ApplicationController
 
     if @transaction.save
       flash[:notice] = "💸 Ton remboursement a été validé!"
-      redirect_to bills_path(query_month: @transaction.bill.payment_date.month)
+      @lydia_url = lydia_request(@transaction.bill.user.phone_number, @transaction.amount.to_s, @transaction.user.phone_number)
+      respond_to do |format|
+        format.html { redirect_to bills_path(query_month: @transaction.bill.payment_date.month) }
+        format.js  # <-- will render `app/views/transcations/create.js.erb`
+      end
     else
-      redirect_to bills_path(query_month: @transaction.bill.payment_date.month)
+      respond_to do |format|
+        format.html { redirect_to bills_path(query_month: @transaction.bill.payment_date.month) }
+        format.js  # <-- idem
+      end
     end
   end
 
-  def lydia_request
+  private
+
+  def lydia_request(recipient, amount, payer_info)
     url = 'https://homologation.lydia-app.com/api/payment/init.json'
     params = {
       provider_token: "5cf67f0ac45e8598414133",
-      recipient: "+33681101126",
-      amount: "12,3",
+      recipient: recipient,
+      amount: amount,
       currency: "EUR",
-      payer_info: "+33763304914"
+      payer_info: payer_info
     }
-    result = RestClient.post(url, params)
-    puts result
-  end
 
-  private
+    result = RestClient.post(url, params)
+    response = JSON.parse result.body
+    response['data']['url']
+  end
 
   def transaction_params
     params.require(:transaction).permit(:user_id, :bill_id)
